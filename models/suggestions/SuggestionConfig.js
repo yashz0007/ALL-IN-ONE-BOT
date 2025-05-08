913d0d31006cfd80c51d2127994831f899e0d1ff
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-05-08 19:27:54 +0530

    Delete models directory

--- models/suggestions/SuggestionConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const suggestionConfigSchema = new mongoose.Schema({
-    guildId: { type: String, required: true, unique: true },
-    suggestionChannelId: { type: String, required: true },
-    allowedRoleId: { type: String, default: null }
-});
-
-module.exports = mongoose.model('SuggestionConfig', suggestionConfigSchema);
--- models/truthordare/TruthOrDareConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const truthOrDareSchema = new mongoose.Schema({
-    serverId: { type: String, required: true, unique: true },
-    channelId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('TruthOrDareConfig', truthOrDareSchema);
