b0b207f4fdeb50862b502aada99a7d93652989a8
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-05-22 17:33:43 +0530

    Add files via upload

---
+++ utils/distubeConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    distubeOptions: {
+        emitAddListWhenCreatingQueue: true,
+        emitAddSongWhenCreatingQueue: false,
+        emitNewSongOnly: true,
+        joinNewVoiceChannel: true,
+        nsfw: true,
+        savePreviousSongs: true,
+    }
+};
