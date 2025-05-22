a3a434d56e172cd2a46c85a00c6cf42aa37f5051
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-05-22 17:29:18 +0530

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,10 @@
 module.exports = {
     enabled: true, 
     lavalink: {
-        name: "GlaceYT",
-        password: "glaceyt",
-        host: "193.226.78.187",
-        port:  3543,
-        secure: false
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "5.39.63.207",
+      port:  8262,
+      secure: false
     }
 };
